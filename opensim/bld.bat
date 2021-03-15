7dd2ffc09146a53e55bb510645158b7872a0ecd3
Vishal Raveendranathan <v.raveendranathan@rug.nl>
2021-03-15 14:17:02 +0100

    Win64 build success.
    It is better to choose swig and cmake of the system rather than conda
    while building the package.

--- opensim/bld.bat
+++ opensim/bld.bat
@@ -1,7 +1,7 @@
 mkdir opensim_dependencies_build
 cd .\opensim_dependencies_build
 cmake ..\dependencies^
-	-G"Visual Studio 14 2015 Win64"^
+	-G"Visual Studio 15 2017 Win64"^
 	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 cmake --build . --config Release -- /maxcpucount:8
 cd .. 
@@ -9,13 +9,16 @@ cd ..
 mkdir opensim_build
 cd .\opensim_build
 cmake ..\^
-	-G"Visual Studio 14 2015 Win64"^
+	-G"Visual Studio 15 2017 Win64"^
 	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
 	-DOPENSIM_DEPENDENCIES_DIR="%LIBRARY_PREFIX%"^
 	-DBUILD_PYTHON_WRAPPING=ON^
-	-DWITH_BTK=ON
+	-DWITH_BTK=ON^
+	-DBUILD_TESTING=OFF^
+	-DBUILD_API_EXAMPLES=OFF
+
 cmake --build . --target install --config Release -- /maxcpucount:8
 
-cp %LIBRARY_PREFIX%\simbody\bin\simbody-visualizer.exe %PREFIX%\simbody-visualizer.exe
+copy %LIBRARY_PREFIX%\simbody\bin\simbody-visualizer.exe %PREFIX%\simbody-visualizer.exe
 cd %LIBRARY_PREFIX%\sdk\python
 python setup.py install
